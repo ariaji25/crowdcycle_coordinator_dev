063600e1628de3e632fd4c6d7df12835b3e32f2b
Ari Purnama <ari.purnama838@gmail.com>
2023-10-30 16:15:49 +0700

    Deploy

---
+++ precache-manifest.528452d7dd6e1f114100b25a980f7009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d3638663ae79119e0e109d8db247b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfffd463853264e6103f",
+    "url": "/static/css/2.48db8b0f.chunk.css"
+  },
+  {
+    "revision": "bd36027b49365aa5163a",
+    "url": "/static/css/main.96e869f2.chunk.css"
+  },
+  {
+    "revision": "bfffd463853264e6103f",
+    "url": "/static/js/2.a6bc141f.chunk.js"
+  },
+  {
+    "revision": "f3e4151181c55821d4a0e75226371454",
+    "url": "/static/js/2.a6bc141f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd36027b49365aa5163a",
+    "url": "/static/js/main.fab5793d.chunk.js"
+  },
+  {
+    "revision": "643a9e6ace48afacc7c5",
+    "url": "/static/js/runtime-main.3326b2e9.js"
+  }
+]);
